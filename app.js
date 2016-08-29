35a699e68bc77b3e8b7519c6496d454f43e948a1
jenniferjara <lisbet.jara@gmail.com>
2016-08-29 08:43:08 -0500

    agrega

---
+++ app.js
@@ -0,0 +1,54 @@
+var opcion = parseInt(prompt("Escoja una opción \n" + 
+	"1. Suma\n" +
+	"2. Resta\n" +
+	"3. Multiplicación\n" + 
+	"4. División\n" + 
+	"5. Comparación\n" +
+	"6. Resto"));
+if(opcion < 7 && opcion>0){
+	var numero1 = parseInt(prompt("Ingrese el #1"));
+	var numero2 = parseInt(prompt("Ingrese el #2"));
+}
+
+switch (opcion){
+	case 1:
+		var suma = numero1 + numero2;
+		document.write("La suma es: " + suma);
+		break;
+	case 2:
+		var resta = numero1 - numero2;
+		document.write("La resta es: " + resta);
+		break;
+	case 3:
+		var multiplica = numero1 * numero2;
+		document.write("La multiplicación es: " + multiplica);
+		break;
+	case 4:
+		var division = numero1 / numero2;
+		document.write("La división es: " + division);
+		break;
+		
+	case 5:
+		if (numero1>numero2){
+			document.write("El "+ numero1 + " es mayor que " + numero2);
+		}else if(numero1==numero2){
+			document.write( numero1 + " es igual a " + numero2);
+		}
+		else {
+			document.write("El "+ numero1 + " es menor que " + numero2);
+		} 
+		break;
+	case 6:
+		if(numero1>numero2 && numero1>0 && numero2>0){
+			var resto=numero1%numero2;
+			document.write("El resto es:"+resto);
+
+		}
+		else{
+			document.write("El resto no exixte");
+		}
+		break;
+	default: 
+		document.write("No es una opción");
+		break;
+}
---
+++ readme.md
@@ -0,0 +1,14 @@
+#Calculadora Simple
+Este repositorio contiene una calculadora simple que le permite al usuario realizar operacionea aritméticas y de comparación.
+A continuación se muestra el psedocódigo y el diagrama de flujo del ejercicio descrito líneas arriba.
+
+##Psedocódigo
+
+1. Inicio: Muestra la lista de de operaciones que puedes elegir.
+2. Elegir la operación.
+3. Ingresar los números.
+3. Se muestra el resultado.
+4. Se muestra la lista de operaciones nuevamente. 
+5. Si el usuario quiere realizar una operación más, ingresar el número de la opción que desee.
+6. De lo contrario ingresar el número de la opción salir.
+7. Fin.
